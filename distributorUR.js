9412598bc9e6c44868fc0451da10af37dfabf512
Wouter Nieuwerth <wouternieuwerth@gmail.com>
2019-12-07 19:15:50 +0100

    Update distributorUR.js

--- distributorUR.js
+++ distributorUR.js
@@ -1,19 +1,55 @@
 const queryObject = [{'query':'lg oled', 'impressions':21129}, {'query':'oled tv', 'impressions':19637}, {'query':'mediamarkt black friday', 'impressions':15870}, {'query':'mobiele airco', 'impressions':15270}, {'query':'lg tv', 'impressions':11923}, {'query':'airco', 'impressions':9554}, {'query':'lg oled55c8pla', 'impressions':7913}, {'query':'miele wasmachine', 'impressions':7333}]
 const feedAsArray = [['merk', 'type'],['Philips', 'HR 183600'],['Dyson', 'ALLERGYKIT'],['Philips', 'FC 806001'],['Samsung', 'RB 31 FERNCSAEF'],['Philips', 'SC 526512'],['Miele', 'HBD 6022 DEKSEL'],['Miele', 'HUB 5000 XLBRAADSLEDE'],['Miele', 'HBF 271 RONDEBAKVORMANTRACIET'],['Miele', 'HBB 51 BAKPLAATANTRACIET'],['Miele', 'HBBR 50 BAKENBRAADROOSTER'],['Miele', 'DGGL 10 STOOMOVENPANMETGATEN'],['Inventum', 'CC 08'],['Inventum', 'HW 508'],['Inventum', 'HNL 4212 Z'],['Tefal', 'RE 4588'],['Braun', 'MQ 700 SOUP'],['Tefal', 'FF 1231'],['Tefal', 'RE 5228']]
+const URL = 'https://europe-west1-cloud-functions-wouter.cloudfunctions.net/fuzzywuzzy_list'
 
 const _ = require('lodash')
+const request = require("request")
+let listOfOptions = []
+
+// Dit is de functie die parallel uitgevoerd gaat worden.
+const asyncOperation = function(options) {
+    return new Promise(function(resolve, reject){
+        request.post(options, callback(err, response, body){
+            if (err) {
+                reject(err)
+            } else {
+                resolve(response)
+            }
+        })
+    })
+};
 
 let feedUnzipped = _.unzip(feedAsArray)
+
 for (var i=0; i<feedUnzipped.length; i++) {
     let column = feedUnzipped[i];
-    var columnUniq = _.uniq(column)
+    let columnUniq = _.uniq(column)
     console.log('-----------')
+    console.log('columnUniq:')
     console.log(columnUniq)
+
+    const payload = {
+        'searchterms': queryObject,
+        'brands': columnUniq
+    }
+    console.log('-----------')
+    console.log('payload:')
+    console.log(payload)
+
+    var options = {
+        url: URL,
+        body: JSON.stringify(payload)
+    }
+
+    listOfOptions.push(options)
 }
 
+// Dit is de functie die uitgevoerd moet worden uiteindelijk. Geeft de resultaten van alle parallel uitgevoerde functies terug.
 async function distribute() {
     // Running Promises in parallel
-    const listOfPromises = listOfArguments.map(asyncOperation);
+    const listOfPromises = listOfOptions.map(asyncOperation);
     // Harvesting
     return await Promise.all(listOfPromises);
 }
+
+console.log(distribute())
